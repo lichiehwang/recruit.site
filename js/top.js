f0d9e7ebf969325c403396bf9b5768081cc00731
lichiehwang <131224887+lichiehwang@users.noreply.github.com>
2024-03-17 21:55:15 +0900

    Update top.js

--- js/top.js
+++ js/top.js
@@ -43,3 +43,14 @@ $(window).on('load', function(){
 		$(Box).slideDown(500);					//アコーディオンを開く
 	});
 });
+$(function(){
+	$(".slideshow-fade li").css({"position":"relative","overflow":"hidden"});
+	$(".slideshow-fade li").hide().css({"position":"absolute","top":0,"left":0});
+	$(".slideshow-fade li:first").addClass("fade").show();
+	setInterval(function(){
+	  var $active = $(".slideshow-fade li.fade");
+	  var $next = $active.next("li").length?$active.next("li"):$(".slideshow-fade li:first");
+	  $active.fadeOut(1000).removeClass("fade");
+	  $next.fadeIn(1000).addClass("fade");
+	},3000);
+  });
